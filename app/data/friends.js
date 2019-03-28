b782784037e2cbcff96c0417373cf88fd0f77003
Arjunkdutta <45476417+Arjunkdutta@users.noreply.github.com>
2019-03-28 12:36:29 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [
+	{
+		name: "Spongebob",
+		photo: "https://vignette.wikia.nocookie.net/joke-battles/images/e/e4/SpongeBob_%285%29.png/revision/latest?cb=20180312154141",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Squidward",
+		photo: "https://banner2.kisspng.com/20180407/pzq/kisspng-squidward-tentacles-plankton-and-karen-drawing-mr-squid-5ac8da0d83fff8.6036971615231124615407.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Larry",
+		photo: "https://vignette.wikia.nocookie.net/spongebob/images/1/1e/Larry_The_Lobster_%28Ripped_Pants%29.jpg/revision/latest?cb=20130320005111",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Sandy",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Sandy_Cheeks.svg/220px-Sandy_Cheeks.svg.png",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Mr.Krabs",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Mr._Krabs.svg/220px-Mr._Krabs.svg.png",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
+
+
